7d2e6504fe7ac24994e43594d34e169c000fd331
Akash Halder <akshalder11@gmail.com>
2023-07-18 17:27:24 +0530

    v1.1

--- src/App.js
+++ src/App.js
@@ -2,32 +2,46 @@ import "./App.css";
 import * as React from "react";
 import Grid from "@mui/material/Grid";
 import Container from "@mui/material/Container";
-import { CalendarOutline, ListOutline } from 'react-ionicons'
+import { CalendarOutline, ListOutline, TimerOutline, ColorWandOutline, CheckmarkDoneCircleOutline } from 'react-ionicons'
 
 
 function App() {
   return (
     <div className="App">
       <div className="header">
-        <p>To-Do List</p>
+        <h1>To-Do List</h1>
       </div>
       {/* <a href="#" className="addTask">
           + Add Task
         </a> */}
-      <Container>
+      <Container className="eachContainer pendingTask">
+        <Grid container className="headGroup">
+          <Grid item xs={12} sm={6} md={6} lg={6}>
+          <div className="sectionHead">
+            <TimerOutline color={'#2f2f2f'} title={'PendingTask'} height="36px" width="36px" />
+            <h2 className="sectionSubHead">Pending Task</h2>
+          </div>
+          </Grid>
+          <Grid item>
+          <div className="sectionHead">
+            <ColorWandOutline color={'#2f2f2f'} title={'AddTask'} height="36px" width="36px" />
+            <h2 className="sectionSubHead">Add Task</h2>
+          </div>
+          </Grid>
+        </Grid>
         <Grid container spacing={{ xs: 2, md: 4 }}>
-        <Grid item xs={12} sm={6} md={4} lg={4}>
+          <Grid item xs={12} sm={6} md={4} lg={4}>
             <div className="card">
               <p className="taskHead">Task One added now</p>
               <p className="taskDesc">Description goes here</p>
               <div className="moreDetails">
                 <div className="iconContainer">
-                <CalendarOutline color={'#2f2f2f'} title={'Calendar'} height="28px" width="28px"/>
-                <p className="values">20th July</p>
+                  <CalendarOutline color={'#2f2f2f'} title={'Calendar'} height="28px" width="28px" />
+                  <p className="values">20th July</p>
                 </div>
                 <div className="iconContainer">
-                <ListOutline color={'#2f2f2f'} title={'SubTask'} height="28px" width="28px"/>
-                <p className="values">12/32 tasks</p>
+                  <ListOutline color={'#2f2f2f'} title={'SubTask'} height="28px" width="28px" />
+                  <p className="values">12/32 tasks</p>
                 </div>
               </div>
               <div className="buttonContainer">
@@ -43,12 +57,12 @@ function App() {
               <p className="taskDesc">Description goes here</p>
               <div className="moreDetails">
                 <div className="iconContainer">
-                <CalendarOutline color={'#2f2f2f'} title={'Calendar'} height="28px" width="28px"/>
-                <p className="values">20th July</p>
+                  <CalendarOutline color={'#2f2f2f'} title={'Calendar'} height="28px" width="28px" />
+                  <p className="values">20th July</p>
                 </div>
                 <div className="iconContainer">
-                <ListOutline color={'#2f2f2f'} title={'SubTask'} height="28px" width="28px"/>
-                <p className="values">12/32 tasks</p>
+                  <ListOutline color={'#2f2f2f'} title={'SubTask'} height="28px" width="28px" />
+                  <p className="values">12/32 tasks</p>
                 </div>
               </div>
               <div className="buttonContainer">
@@ -64,12 +78,12 @@ function App() {
               <p className="taskDesc">Description goes here</p>
               <div className="moreDetails">
                 <div className="iconContainer">
-                <CalendarOutline color={'#2f2f2f'} title={'Calendar'} height="28px" width="28px"/>
-                <p className="values">20th July</p>
+                  <CalendarOutline color={'#2f2f2f'} title={'Calendar'} height="28px" width="28px" />
+                  <p className="values">20th July</p>
                 </div>
                 <div className="iconContainer">
-                <ListOutline color={'#2f2f2f'} title={'SubTask'} height="28px" width="28px"/>
-                <p className="values">12/32 tasks</p>
+                  <ListOutline color={'#2f2f2f'} title={'SubTask'} height="28px" width="28px" />
+                  <p className="values">12/32 tasks</p>
                 </div>
               </div>
               <div className="buttonContainer">
@@ -85,12 +99,12 @@ function App() {
               <p className="taskDesc">Description goes here</p>
               <div className="moreDetails">
                 <div className="iconContainer">
-                <CalendarOutline color={'#2f2f2f'} title={'Calendar'} height="28px" width="28px"/>
-                <p className="values">20th July</p>
+                  <CalendarOutline color={'#2f2f2f'} title={'Calendar'} height="28px" width="28px" />
+                  <p className="values">20th July</p>
                 </div>
                 <div className="iconContainer">
-                <ListOutline color={'#2f2f2f'} title={'SubTask'} height="28px" width="28px"/>
-                <p className="values">12/32 tasks</p>
+                  <ListOutline color={'#2f2f2f'} title={'SubTask'} height="28px" width="28px" />
+                  <p className="values">12/32 tasks</p>
                 </div>
               </div>
               <div className="buttonContainer">
@@ -106,12 +120,12 @@ function App() {
               <p className="taskDesc">Description goes here</p>
               <div className="moreDetails">
                 <div className="iconContainer">
-                <CalendarOutline color={'#2f2f2f'} title={'Calendar'} height="28px" width="28px"/>
-                <p className="values">20th July</p>
+                  <CalendarOutline color={'#2f2f2f'} title={'Calendar'} height="28px" width="28px" />
+                  <p className="values">20th July</p>
                 </div>
                 <div className="iconContainer">
-                <ListOutline color={'#2f2f2f'} title={'SubTask'} height="28px" width="28px"/>
-                <p className="values">12/32 tasks</p>
+                  <ListOutline color={'#2f2f2f'} title={'SubTask'} height="28px" width="28px" />
+                  <p className="values">12/32 tasks</p>
                 </div>
               </div>
               <div className="buttonContainer">
@@ -127,12 +141,12 @@ function App() {
               <p className="taskDesc">Description goes here</p>
               <div className="moreDetails">
                 <div className="iconContainer">
-                <CalendarOutline color={'#2f2f2f'} title={'Calendar'} height="28px" width="28px"/>
-                <p className="values">20th July</p>
+                  <CalendarOutline color={'#2f2f2f'} title={'Calendar'} height="28px" width="28px" />
+                  <p className="values">20th July</p>
                 </div>
                 <div className="iconContainer">
-                <ListOutline color={'#2f2f2f'} title={'SubTask'} height="28px" width="28px"/>
-                <p className="values">12/32 tasks</p>
+                  <ListOutline color={'#2f2f2f'} title={'SubTask'} height="28px" width="28px" />
+                  <p className="values">12/32 tasks</p>
                 </div>
               </div>
               <div className="buttonContainer">
@@ -148,12 +162,12 @@ function App() {
               <p className="taskDesc">Description goes here</p>
               <div className="moreDetails">
                 <div className="iconContainer">
-                <CalendarOutline color={'#2f2f2f'} title={'Calendar'} height="28px" width="28px"/>
-                <p className="values">20th July</p>
+                  <CalendarOutline color={'#2f2f2f'} title={'Calendar'} height="28px" width="28px" />
+                  <p className="values">20th July</p>
                 </div>
                 <div className="iconContainer">
-                <ListOutline color={'#2f2f2f'} title={'SubTask'} height="28px" width="28px"/>
-                <p className="values">12/32 tasks</p>
+                  <ListOutline color={'#2f2f2f'} title={'SubTask'} height="28px" width="28px" />
+                  <p className="values">12/32 tasks</p>
                 </div>
               </div>
               <div className="buttonContainer">
@@ -169,12 +183,12 @@ function App() {
               <p className="taskDesc">Description goes here</p>
               <div className="moreDetails">
                 <div className="iconContainer">
-                <CalendarOutline color={'#2f2f2f'} title={'Calendar'} height="28px" width="28px"/>
-                <p className="values">20th July</p>
+                  <CalendarOutline color={'#2f2f2f'} title={'Calendar'} height="28px" width="28px" />
+                  <p className="values">20th July</p>
                 </div>
                 <div className="iconContainer">
-                <ListOutline color={'#2f2f2f'} title={'SubTask'} height="28px" width="28px"/>
-                <p className="values">12/32 tasks</p>
+                  <ListOutline color={'#2f2f2f'} title={'SubTask'} height="28px" width="28px" />
+                  <p className="values">12/32 tasks</p>
                 </div>
               </div>
               <div className="buttonContainer">
@@ -184,6 +198,141 @@ function App() {
               </div>
             </div>
           </Grid>
+        </Grid>
+      </Container>
+      <Container className="eachContainer completedTask">
+        <div className="sectionHead">
+          <CheckmarkDoneCircleOutline color={'#2f2f2f'} title={'CompletedTask'} height="36px" width="36px" />
+          <h2 className="sectionSubHead">Completed Task</h2>
+        </div>
+        <Grid container spacing={{ xs: 2, md: 4 }}>
+          <Grid item xs={12} sm={6} md={4} lg={4}>
+            <div className="card">
+              <p className="taskHead">Task One added now</p>
+              <p className="taskDesc">Description goes here</p>
+              <div className="moreDetails">
+                <div className="iconContainer">
+                  <CalendarOutline color={'#2f2f2f'} title={'Calendar'} height="28px" width="28px" />
+                  <p className="values">20th July</p>
+                </div>
+                <div className="iconContainer">
+                  <ListOutline color={'#2f2f2f'} title={'SubTask'} height="28px" width="28px" />
+                  <p className="values">12/32 tasks</p>
+                </div>
+              </div>
+              <div className="buttonContainer">
+                <div className="btn done-btn">Mark As Undone</div>
+                <div className="btn edit-btn">Edit</div>
+                <div className="btn delete-btn">Delete</div>
+              </div>
+            </div>
+          </Grid>
+          <Grid item xs={12} sm={6} md={4} lg={4}>
+            <div className="card">
+              <p className="taskHead">Task One added now</p>
+              <p className="taskDesc">Description goes here</p>
+              <div className="moreDetails">
+                <div className="iconContainer">
+                  <CalendarOutline color={'#2f2f2f'} title={'Calendar'} height="28px" width="28px" />
+                  <p className="values">20th July</p>
+                </div>
+                <div className="iconContainer">
+                  <ListOutline color={'#2f2f2f'} title={'SubTask'} height="28px" width="28px" />
+                  <p className="values">12/32 tasks</p>
+                </div>
+              </div>
+              <div className="buttonContainer">
+                <div className="btn done-btn">Mark As Undone</div>
+                <div className="btn edit-btn">Edit</div>
+                <div className="btn delete-btn">Delete</div>
+              </div>
+            </div>
+          </Grid>
+          <Grid item xs={12} sm={6} md={4} lg={4}>
+            <div className="card">
+              <p className="taskHead">Task One added now</p>
+              <p className="taskDesc">Description goes here</p>
+              <div className="moreDetails">
+                <div className="iconContainer">
+                  <CalendarOutline color={'#2f2f2f'} title={'Calendar'} height="28px" width="28px" />
+                  <p className="values">20th July</p>
+                </div>
+                <div className="iconContainer">
+                  <ListOutline color={'#2f2f2f'} title={'SubTask'} height="28px" width="28px" />
+                  <p className="values">12/32 tasks</p>
+                </div>
+              </div>
+              <div className="buttonContainer">
+                <div className="btn done-btn">Mark As Undone</div>
+                <div className="btn edit-btn">Edit</div>
+                <div className="btn delete-btn">Delete</div>
+              </div>
+            </div>
+          </Grid>
+          <Grid item xs={12} sm={6} md={4} lg={4}>
+            <div className="card">
+              <p className="taskHead">Task One added now</p>
+              <p className="taskDesc">Description goes here</p>
+              <div className="moreDetails">
+                <div className="iconContainer">
+                  <CalendarOutline color={'#2f2f2f'} title={'Calendar'} height="28px" width="28px" />
+                  <p className="values">20th July</p>
+                </div>
+                <div className="iconContainer">
+                  <ListOutline color={'#2f2f2f'} title={'SubTask'} height="28px" width="28px" />
+                  <p className="values">12/32 tasks</p>
+                </div>
+              </div>
+              <div className="buttonContainer">
+                <div className="btn done-btn">Mark As Undone</div>
+                <div className="btn edit-btn">Edit</div>
+                <div className="btn delete-btn">Delete</div>
+              </div>
+            </div>
+          </Grid>
+          <Grid item xs={12} sm={6} md={4} lg={4}>
+            <div className="card">
+              <p className="taskHead">Task One added now</p>
+              <p className="taskDesc">Description goes here</p>
+              <div className="moreDetails">
+                <div className="iconContainer">
+                  <CalendarOutline color={'#2f2f2f'} title={'Calendar'} height="28px" width="28px" />
+                  <p className="values">20th July</p>
+                </div>
+                <div className="iconContainer">
+                  <ListOutline color={'#2f2f2f'} title={'SubTask'} height="28px" width="28px" />
+                  <p className="values">12/32 tasks</p>
+                </div>
+              </div>
+              <div className="buttonContainer">
+                <div className="btn done-btn">Mark As Undone</div>
+                <div className="btn edit-btn">Edit</div>
+                <div className="btn delete-btn">Delete</div>
+              </div>
+            </div>
+          </Grid>
+          <Grid item xs={12} sm={6} md={4} lg={4}>
+            <div className="card">
+              <p className="taskHead">Task One added now</p>
+              <p className="taskDesc">Description goes here</p>
+              <div className="moreDetails">
+                <div className="iconContainer">
+                  <CalendarOutline color={'#2f2f2f'} title={'Calendar'} height="28px" width="28px" />
+                  <p className="values">20th July</p>
+                </div>
+                <div className="iconContainer">
+                  <ListOutline color={'#2f2f2f'} title={'SubTask'} height="28px" width="28px" />
+                  <p className="values">12/32 tasks</p>
+                </div>
+              </div>
+              <div className="buttonContainer">
+                <div className="btn done-btn">Mark As Undone</div>
+                <div className="btn edit-btn">Edit</div>
+                <div className="btn delete-btn">Delete</div>
+              </div>
+            </div>
+          </Grid>
+
         </Grid>
       </Container>
     </div>
